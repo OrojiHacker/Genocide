c8e5c43502a75448cf558c84d372088db9bc8379
OrojiHacker <52883876+OrojiHacker@users.noreply.github.com>
2019-08-03 14:15:05 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo
+echo "\033[1m\033[33m รอแปป"
+echo
+apt-get install toilet -y > /dev/null 2>&1
+apt-get install python2 -y > /dev/null 2>&1
+apt-get install figlet -y > /dev/null 2>&1
+apt-get install php -y > /dev/null 2>&1
+echo
+echo "\033[1m\033[32m เสร็จละ"
+echo "\033[0m"
+python2 Genocide.py
